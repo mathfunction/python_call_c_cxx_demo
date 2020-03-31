02833c8ed229721711787f54a2d63e20819b672c
tommy_cheng <tommy_cheng@bb.com>
2020-03-31 15:31:28 +0800

    20200331

--- README.md
+++ README.md
@@ -1,20 +1,40 @@
-## [開發中] Python_Call_C/C++_Demo
+## Python_Call_C/C++_Demo
 
-#### 建立自訂編譯的管道
+這是關於 Python 使用 C/C++ 加速如何編譯系列:
 
 
 
 - cython
 
-  ​	
-
-
-
-
-
-
+  | 作業系統    | c/c++ 編譯器                             |
+  | ----------- | ---------------------------------------- |
+  | Linux       | [GNU gcc/g++](https://gcc.gnu.org/)      |
+  | Max(Darwin) | [clang/clang++](https://clang.llvm.org/) |
+  | Windows     | [MSYS2 gcc/g++](https://www.msys2.org/)  |
 
 - ctypes
+
+  | 作業系統    | c/c++ 編譯器                                                 |
+  | ----------- | ------------------------------------------------------------ |
+  | Linux       | [GNU gcc/g++](https://gcc.gnu.org/)                          |
+  | Mac(Darwin) | [clang/clang++](https://clang.llvm.org/)                     |
+  | Windows     | [MSVC(Microsoft Visual Studio) cl](https://docs.microsoft.com/zh-tw/cpp/build/reference/compiler-options?view=vs-2019) |
+
+
+
+
+
+
+
+##### 編譯器 command line option 其他參考:
+
+- GNU gcc  <https://gcc.gnu.org/onlinedocs/gcc/Invoking-GCC.html#Invoking-GCC>
+
+- clang <https://clang.llvm.org/docs/ClangCommandLineReference.html#diagnostic-flags> 
+
+- MSVC <https://github.com/MicrosoftDocs/cpp-docs/blob/master/docs/build/reference/compiler-options.md>
+
+
 
 
 
--- compileDIY.py
+++ compileDIY.py
@@ -4,14 +4,20 @@
 
 		
 		- compiler 預設 :
-			Linux :  GNU gcc/g++
-			Max(Darwin) : clang/clang++
+			Linux :  
+				GNU gcc/g++ - https://gcc.gnu.org/
+
+			Max(Darwin) : 
+				clang/clang++ - https://clang.llvm.org/
 			Windows :  
-				cython : MSYS2 : gcc.exe
-				ctypes : MSVC :  cl.exe
+				[cython]  MSYS2 gcc.exe (anaconda default) - https://www.msys2.org/
+				[ctypes]  MSVC : cl.exe - https://docs.microsoft.com/zh-tw/cpp/build/reference/compiler-options?view=vs-2019
 	
 		- commands 部分實作，需自行連結函式庫 !! 
-	
+		
+
+
+		- flag options 說明連結 
 
 
 
@@ -19,184 +25,177 @@
 ============================================================================================================================================================="""
 import os 
 import platform
-import subprocess
 import shutil
 
-class ComplieDIY:
-	def __init__(self):
-		# 回傳作業系統名稱  Windows/Darwin(Mac)/Linux
-		self.PlatformName = platform.system()
-	#======================================================================================================================================================================================================================
-	# cython
-	def cython_copy2pyx(self,pyfile,source):
-	    pyfilename1 = "{}{}".format(source,pyfile)
-	    pyfilename2 = pyfile
-	    shutil.copyfile(pyfilename1,pyfilename2)
-	    pyfilename3 = pyfilename2.split(".py")[0]+".pyx"
-	    try:
-	    	os.remove(pyfilename3)
-	    except:
-	    	pass
-	    os.rename(pyfilename2,pyfilename3)
-
-	    print("copy + rename {} to {} !!".format(pyfilename1,pyfilename3))
-	    return pyfilename3
-
-
-	def cython_c2o(self,cfile):
-		if self.PlatformName == "Linux":
-			ofile = "{}.o".format(cfile.split(".c")[0])
-			commands = [
-				"gcc",
-				"-pthread",
-				"-DNDEBUG",
-				"-g",
-				"-fwrapv",
-				"-O2",
-				"-Wall",
-				"-g",
-				"-fstack-protector-strong", 
-				"-Wformat",
-				"-Werror=format-security",
-				"-Wdate-time",
-				"-D_FORTIFY_SOURCE=2",
-				"-fPIC",
-				"-I/usr/include/python3.6m",  # python3.6m
-				"-c",
-				cfile,
-				"-o",
-				ofile
-			]
-			subprocess.run(commands)
-			print(" ".join(commands))
-			return ofile 
-
-		elif self.PlatformName == "Darwin":
-			ofile = "{}.o".format(cfile.split(".c")[0])
-			commands = [
-				"clang",
-				"-Wno-unused-result",
-				"-Wsign-compare",
-				"-Wunreachable-code",
-				"-fno-common",
-				"-dynamic",
-				"-DNDEBUG",
-				"-g", 
-				"-fwrapv", 
-				"-O3", 
-				"-Wall", 
-				"-isysroot",
-				"/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk",
-				"-I/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk/usr/include", 
-				"-I/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk/System/Library/Frameworks/Tk.framework/Versions/8.5/Headers", 
-				"-I/usr/local/include -I/usr/local/opt/openssl/include", 
-				"-I/usr/local/opt/sqlite/include", 
-				"-I/usr/local/Cellar/python/3.7.4/Frameworks/Python.framework/Versions/3.7/include/python3.7m", 
-				"-c", 
-				cfile, 
-				"-o", 
-				ofile
-			]
-			subprocess.run(commands)
-			print(" ".join(commands))
-			return ofile
-		
-		elif self.PlatformName == "Windows":
-			ofile = "{}.o".format(cfile.split(".c")[0])
-			commands = [
-				"gcc.exe",
-				"-mdll",
-				"-O", 
-				"-Wall", 
-				"-DMS_WIN64",
-				"-IC:\\ProgramData\\Anaconda3\\include", 
-				"-IC:\\ProgramData\\Anaconda3\\include",
-				"-c", 
-				cfile, 
-				"-o",
-				ofile
-			]
-			subprocess.run(commands)
-			print(" ".join(commands))
-			return ofile
-		else:
-			exit()
-
-
-	def cython_o2so(self,ofile):
-		if self.PlatformName == "Linux":
-			sofile = "{}.so".format(ofile.split(".o")[0])
-			commands = [
-				"gcc",
-				"-pthread",
-				"-shared",
-				"-Wl,-O1",
-				"-Wl,-Bsymbolic-functions",
-				"-Wl,-Bsymbolic-functions",
-				"-Wl,-z,relro",
-				"-Wl,-Bsymbolic-functions",
-				"-Wl,-z,relro",
-				"-g",
-				"-fstack-protector-strong",
-				"-Wformat",
-				"-Werror=format-security",
-				"-Wdate-time",
-				"-D_FORTIFY_SOURCE=2",
-				ofile,
-				"-o",
-				sofile
-			]
-			subprocess.run(commands)
-			print(" ".join(commands))
-			return sofile	
-		elif self.PlatformName == "Darwin":
-			sofile = "{}.so".format(ofile.split(".o")[0])
-			commands = [
-				"clang",
-				"-bundle",
-				"-undefined", 
-				"dynamic_lookup",
-				"-isysroot",
-				"/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk",
-				ofile,
-				"-L/usr/local/lib", 
-				"-L/usr/local/opt/openssl/lib", 
-				"-L/usr/local/opt/sqlite/lib",
-				"-o",
-				sofile
-			]
-			subprocess.run(commands)
-			print(" ".join(commands))
-			return sofile	
-		elif self.PlatformName == "Windows":
-			#deffile = "{}.def".format(ofile.split(".o")[0])
-			pydfile = "{}.pyd".format(ofile.split(".o")[0])
-			commands = [
-				"gcc.exe",
-				"-shared", 
-				"-s", 
-				ofile,
-				"-LC:\\ProgramData\\Anaconda3\\libs",
-				"-LC:\\ProgramData\\Anaconda3\\PCbuild\\amd64", 
-				"-lpython36", 
-				"-lmsvcr140",
-				"-o",
-				pydfile
-			]
-			subprocess.run(commands)
-			print(" ".join(commands))
-			return pydfile
-		else:
-			exit()
+PLATFORM_NAME = platform.system()
+	
 		
 
-	def cython(self,pyfile,source="../"):
-		pyxfile = self.cython_copy2pyx(pyfile,source=source)
-		subprocess.run(["cython","-3",pyxfile]) # python3
-		cfile = pyfile.split(".py")[0]+".c"
-		ofile = self.cython_c2o(cfile)
-		sofile = self.cython_o2so(ofile)
-		print("[cython] {} --> {} --> {} --> {} --> {}".format(pyfile,pyxfile,cfile,ofile,sofile))
+#======================================================================================================================================================================================================================
+# cython
+def cython_copy2pyx(pyfile,source):
+	pyfilename1 = "{}{}".format(source,pyfile)
+	pyfilename2 = pyfile
+	shutil.copyfile(pyfilename1,pyfilename2)
+	pyfilename3 = pyfilename2.split(".py")[0]+".pyx"
+	try:
+		os.remove(pyfilename3)
+		print("remove {}".format(pyfilename3))
+	except:
+		pass
+	os.rename(pyfilename2,pyfilename3)
+	print("copy + rename {} to {} !!".format(pyfilename1,pyfilename3))
+	return pyfilename3
+
+
+def cython_c2o(cfile):
+	if PLATFORM_NAME == "Linux":
+		ofile = "{}.o".format(cfile.split(".c")[0])
+		commands = [
+			"gcc",
+			"-pthread",
+			"-DNDEBUG",
+			"-g",
+			"-fwrapv",
+			"-O2",
+			"-Wall",
+			"-g",
+			"-fstack-protector-strong", 
+			"-Wformat",
+			"-Werror=format-security",
+			"-Wdate-time",
+			"-D_FORTIFY_SOURCE=2",
+			"-fPIC",
+			"-I/usr/include/python3.6m",  # python3.6m
+			"-c",
+			cfile,
+			"-o",
+			ofile
+		]
+		os.system(" ".join(commands))
+		return ofile 
+
+	elif PLATFORM_NAME == "Darwin":
+		ofile = "{}.o".format(cfile.split(".c")[0])
+		commands = [
+			"clang",
+			"-Wno-unused-result",
+			"-Wsign-compare",
+			"-Wunreachable-code",
+			"-fno-common",
+			"-dynamic",
+			"-DNDEBUG",
+			"-g", 
+			"-fwrapv", 
+			"-O3", 
+			"-Wall", 
+			"-isysroot",
+			"/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk",
+			"-I/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk/usr/include", 
+			"-I/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk/System/Library/Frameworks/Tk.framework/Versions/8.5/Headers", 
+			"-I/usr/local/include -I/usr/local/opt/openssl/include", 
+			"-I/usr/local/opt/sqlite/include", 
+			"-I/usr/local/Cellar/python/3.7.4/Frameworks/Python.framework/Versions/3.7/include/python3.7m", 
+			"-c", 
+			cfile, 
+			"-o", 
+			ofile
+		]
+		os.system(" ".join(commands))
+		return ofile
+		
+	elif PLATFORM_NAME == "Windows":
+		ofile = "{}.o".format(cfile.split(".c")[0])
+		commands = [
+			"gcc.exe",
+			"-mdll",
+			"-O", 
+			"-Wall", 
+			"-DMS_WIN64",
+			"-IC:\\ProgramData\\Anaconda3\\include", 
+			"-IC:\\ProgramData\\Anaconda3\\include",
+			"-c", 
+			cfile, 
+			"-o",
+			ofile
+		]
+		os.system(" ".join(commands))
+		return ofile
+	else:
+		exit()
+
+
+def cython_o2so(ofile):
+	if PLATFORM_NAME == "Linux":
+		sofile = "{}.so".format(ofile.split(".o")[0])
+		commands = [
+			"gcc",
+			"-pthread",
+			"-shared",
+			"-Wl,-O1",
+			"-Wl,-Bsymbolic-functions",
+			"-Wl,-Bsymbolic-functions",
+			"-Wl,-z,relro",
+			"-Wl,-Bsymbolic-functions",
+			"-Wl,-z,relro",
+			"-g",
+			"-fstack-protector-strong",
+			"-Wformat",
+			"-Werror=format-security",
+			"-Wdate-time",
+			"-D_FORTIFY_SOURCE=2",
+			ofile,
+			"-o",
+			sofile
+		]
+		os.system(" ".join(commands))
+		return sofile	
+	elif PLATFORM_NAME == "Darwin":
+		sofile = "{}.so".format(ofile.split(".o")[0])
+		commands = [
+			"clang",
+			"-bundle",
+			"-undefined", 
+			"dynamic_lookup",
+			"-isysroot",
+			"/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk",
+			ofile,
+			"-L/usr/local/lib", 
+			"-L/usr/local/opt/openssl/lib", 
+			"-L/usr/local/opt/sqlite/lib",
+			"-o",
+			sofile
+		]
+		os.system(" ".join(commands))
+		return sofile	
+	elif PLATFORM_NAME == "Windows":
+		#deffile = "{}.def".format(ofile.split(".o")[0])
+		pydfile = "{}.pyd".format(ofile.split(".o")[0])
+		commands = [
+			"gcc.exe",
+			"-shared", 
+			"-s", 
+			ofile,
+			"-LC:\\ProgramData\\Anaconda3\\libs",
+			"-LC:\\ProgramData\\Anaconda3\\PCbuild\\amd64", 
+			"-lpython36", 
+			"-lmsvcr140",
+			"-o",
+			pydfile
+		]
+		os.system(" ".join(commands))
+		return pydfile
+	else:
+		exit()
+		
+
+def cython_compile(pyfile,source="../python/"):
+	pyxfile = cython_copy2pyx(pyfile,source=source)
+	os.system(" ".join(["cython","-3",pyxfile])) # python3
+	cfile = pyfile.split(".py")[0]+".c"
+	ofile = cython_c2o(cfile)
+	sofile = cython_o2so(ofile)
+	print("[cython_compile] {} --> {} --> {} --> {} --> {}".format(pyfile,pyxfile,cfile,ofile,sofile))
 
 
 	
@@ -204,141 +203,147 @@ class ComplieDIY:
 	
 
 
-	#======================================================================================================================================================================================================================
-	# ctypes 系列
-	def ctypes_cpp2o(self,cppfile):
-		if self.PlatformName == "Linux":
-			ofile = "{}.o".format(cppfile.split(".cpp")[0])
-			commands = [
-				"gcc",
-				"-pthread",
-				"-DNDEBUG",
-				"-g",
-				"-fwrapv",
-				"-O2",
-				"-Wall",
-				"-g",
-				"-fstack-protector-strong",
-				"-Wformat",
-				"-Werror=format-security",
-				"-Wdate-time",
-				"-D_FORTIFY_SOURCE=2", 
-				"-fPIC",
-				"-I.",
-				"-I/usr/include/python3.6m",
-				"-c",
-				cppfile,
-				"-o",
-				ofile,
-				"-std=c++11"
-			]
-			print(" ".join(commands))
-			subprocess.run(commands)
-			return ofile
-		elif self.PlatformsName == "Darwin":
-			ofile = "{}.o".format(cppfile.split(".cpp")[0])
-			commands = [
-				"clang",
-				"-Wno-unused-result",
-				"-Wsign-compare",
-				"-Wunreachable-code",
-				"-fno-common",
-				"-dynamic",
-				"-DNDEBUG",
-				"-g",
-				"-fwrapv",
-				"-O3",
-				"-Wall",
-				"-isysroot",
-				"/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk",
-				"-I/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk/usr/include", 
-				"-I/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk/System/Library/Frameworks/Tk.framework/Versions/8.5/Headers",
-				"-I.",
-				"-I/usr/local/include",
-				"-I/usr/local/opt/openssl/include", 
-				"-I/usr/local/opt/sqlite/include",
-				"-I/usr/local/Cellar/python/3.7.4/Frameworks/Python.framework/Versions/3.7/include/python3.7m",
-				"-c",
-				cppfile, 
-				"-o", 
-				ofile,
-				"-std=c++11"
-			]
-			subprocess.run(commands)
-			print(" ".join(commands))
-			return ofile
-		elif self.PlatformsName == "Windows":
-			pass
+#======================================================================================================================================================================================================================
+# ctypes 系列
+def ctypes_cpp2o(cppfile):
+	if PLATFORM_NAME == "Linux":
+		ofile = "{}.o".format(cppfile.split(".cpp")[0])
+		commands = [
+			"gcc",
+			"-pthread",
+			"-DNDEBUG",
+			"-g",
+			"-fwrapv",
+			"-O2",
+			"-Wall",
+			"-g",
+			"-fstack-protector-strong",
+			"-Wformat",
+			"-Werror=format-security",
+			"-Wdate-time",
+			"-D_FORTIFY_SOURCE=2", 
+			"-fPIC",
+			"-I.",
+			"-I/usr/include/python3.6m",
+			"-c",
+			cppfile,
+			"-o",
+			ofile,
+			"-std=c++11"
+		]
+		os.system(" ".join(commands))
+		return ofile
+	elif PlatformsName == "Darwin":
+		ofile = "{}.o".format(cppfile.split(".cpp")[0])
+		commands = [
+			"clang",
+			"-Wno-unused-result",
+			"-Wsign-compare",
+			"-Wunreachable-code",
+			"-fno-common",
+			"-dynamic",
+			"-DNDEBUG",
+			"-g",
+			"-fwrapv",
+			"-O3",
+			"-Wall",
+			"-isysroot",
+			"/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk",
+			"-I/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk/usr/include", 
+			"-I/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk/System/Library/Frameworks/Tk.framework/Versions/8.5/Headers",
+			"-I.",
+			"-I/usr/local/include",
+			"-I/usr/local/opt/openssl/include", 
+			"-I/usr/local/opt/sqlite/include",
+			"-I/usr/local/Cellar/python/3.7.4/Frameworks/Python.framework/Versions/3.7/include/python3.7m",
+			"-c",
+			cppfile, 
+			"-o", 
+			ofile,
+			"-std=c++11"
+		]
+		os.system(" ".join(commands))
+		return ofile
+
+	else:
+		exit()
 
 
 
-
-		else:
-			exit()
-
-
-
-	def ctypes_o2so(self,ofile):
-		if self.PlatformName == "Linux":
-			sofile = "{}.so".format(ofile.split(".o")[0])
-			commands = [
-				"g++",
-				"-pthread",
-				"-shared",
-				"-Wl,-O1",
-				"-Wl,-Bsymbolic-functions",
-				"-Wl,-Bsymbolic-functions", 
-				"-Wl,-z,relro",
-				"-Wl,-Bsymbolic-functions",
-				"-Wl,-z,relro",
-				"-g",
-				"-fstack-protector-strong",
-				"-Wformat",
-				"-Werror=format-security",
-				"-Wdate-time",
-				"-D_FORTIFY_SOURCE=2",
-				ofile,
-				"-o",
-				sofile
-			]
-			print(" ".join(commands))
-			subprocess.run(commands)
-			return sofile
-		elif self.PlatformName == "Darwin":
-			sofile = "{}.so".format(ofile.split(".o")[0])
-			commands = [
-				"clang++",
-				"-bundle",
-				"-undefined",
-				"dynamic_lookup",
-				"-isysroot",
-				"/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk",
-				ofile,
-				"-L/usr/local/lib",
-				"-L/usr/local/opt/openssl/lib",
-				"-L/usr/local/opt/sqlite/lib",
-				"-o",
-				sofile
-			]
-			print(" ".join(commands))
-			subprocess.run(commands)
-			return sofile
+def ctypes_o2so(ofile):
+	if PLATFORM_NAME == "Linux":
+		sofile = "{}.so".format(ofile.split(".o")[0])
+		commands = [
+			"g++",
+			"-pthread",
+			"-shared",
+			"-Wl,-O1",
+			"-Wl,-Bsymbolic-functions",
+			"-Wl,-Bsymbolic-functions", 
+			"-Wl,-z,relro",
+			"-Wl,-Bsymbolic-functions",
+			"-Wl,-z,relro",
+			"-g",
+			"-fstack-protector-strong",
+			"-Wformat",
+			"-Werror=format-security",
+			"-Wdate-time",
+			"-D_FORTIFY_SOURCE=2",
+			ofile,
+			"-o",
+			sofile
+		]
+		os.system(" ".join(commands))
+		return sofile
+	elif PLATFORM_NAME == "Darwin":
+		sofile = "{}.so".format(ofile.split(".o")[0])
+		commands = [
+			"clang++",
+			"-bundle",
+			"-undefined",
+			"dynamic_lookup",
+			"-isysroot",
+			"/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk",
+			ofile,
+			"-L/usr/local/lib",
+			"-L/usr/local/opt/openssl/lib",
+			"-L/usr/local/opt/sqlite/lib",
+			"-o",
+			sofile
+		]
+		os.system(" ".join(commands))
+		return sofile
 		
-		elif self.PlatformName == "Windows":
-			pass
+	else:
+		exit()
 
 
 
-
-		else:
-			exit()
-
-
-
-	def ctypes(self,cppfile):
-		ofile = self.ctypes_cpp2o(cppfile)
-		sofile = self.ctypes_o2so(ofile)
-		print("[ctypes] {} --> {} --> {}".format(cppfile,ofile,sofile))
+def ctypes_compile(cppfile):
+	# 啟動 msvc x86_64  
+	if PLATFORM_NAME == "Windows":
+		os.system("C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VC\\bin\\x86_amd64\\vcvarsx86_amd64.bat")
+		dllfile = "{}.dll".split(cppfile.split(".cpp"))[0]
+		commands = [
+			"cl",
+			"/std:c++14", 
+			"/EHsc", 
+			"/D_ITERATOR_DEBUG_LEVEL=0", 
+			"/O2",
+			cppfile,
+			#"/I",
+			#/link,
+			#/libpath:,
+			#/DLL,
+			"/OUT:",
+			dllfile
+		]
+		os.system(" ".join(commands))
+		print("[ctypes_compile] {} --> {} ".format(cppfile,dllfile))
+	else:
+		ofile = ctypes_cpp2o(cppfile)
+		sofile = ctypes_o2so(ofile)
+		print("[ctypes_compile] {} --> {} --> {}".format(cppfile,ofile,sofile))
 
 
 
